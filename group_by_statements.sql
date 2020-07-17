2adf0f748cd50bcc268c546890f0065735b12be0
hugodmorais <hugodanielmorais@gmail.com>
2020-07-17 22:21:15 +0100

    Having

--- group_by_statements.sql
+++ group_by_statements.sql
@@ -34,3 +34,15 @@ GROUP BY rating;
 SELECT customer_id, SUM(amount) FROM payment
 GROUP BY customer_id
 HAVING SUM(amount) > 100
+
+-- HAVING
+SELECT customer_id, COUNT(*) 
+FROM payment
+GROUP BY customer_id
+HAVING COUNT(*) >= 40;
+
+-- HAVING
+SELECT customer_id, SUM(amount)
+FROM paymentWHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) > 100
