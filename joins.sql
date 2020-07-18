57d083068c3c6e504cf4b418438b502ff64636c3
hugodmorais <hugodanielmorais@gmail.com>
2020-07-18 17:00:31 +0100

    Joins

--- joins.sql
+++ joins.sql
@@ -17,3 +17,26 @@ HAVING  SUM(amount) > 100
 SELECT customer_id, amount AS total_spent
 FROM payment
 WHERE amount > 2
+
+SELECT * 
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id
+
+SELECT * FROM Registrations
+LEFT OUTER JOIN Logins
+ON Registrations.name = Logins.name
+
+SELECT district, email
+FROM address
+INNER JOIN customer
+ON address.address_id = customer.address_id
+WHERE district = 'California'
+
+SELECT title, first_name, last_name FROM actor
+INNER JOIN film_actor
+ON actor.actor_id = film_actor.actor_id
+INNER JOIN film
+ON film_actor.film_id = film.film_id
+WHERE first_name = 'Nick' AND
+last_name = 'Wahlberg'
