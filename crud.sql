3932ab610eeb8f4432206ef95c7da6e1b57f612a
hugodmorais <hugodanielmorais@gmail.com>
2020-07-19 23:09:38 +0100

    Insert, Update and Delete

--- crud.sql
+++ crud.sql
@@ -9,3 +9,24 @@ CREATE TABLE players (
   player_id SERIAL PRIMARY KEY,
   age SMALLINT NOT NULL
 );
+
+INSERT INTO table (column1,column2,...)
+SELECT column1,column2,...
+FROM another_table
+WHERE condition;
+
+INSERT INTO account (username,password,email,created_on)
+VALUES
+('Jose','password','jose@mail.com',CURRENT_TIMESTAMP)
+
+UPDATE table 
+SET colum1 = value1,
+    column2 = value2,...
+WHERE 
+  consition;
+
+DELETE FROM table
+WHERE row_id = 1
+
+
+
